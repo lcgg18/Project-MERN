729da28f94682b8fe55965c1e041985a3833b788
Andres Betancourt <andresbetancourtrc@gmail.com>
2021-09-12 08:53:52 -0500

    entrega funcional del metodo post para guardar un objeto en la base de datos

--- README.md
+++ README.md
@@ -6,4 +6,6 @@ Este proyecto es una introducción a la creación de un backend con node y mongo
 
 el archivo index.js es quien tiene la conexcion a la base de datos y la configuración del servidor 
 
-el archivo app.js tiene la rutas y los midlewares
+### Descripción del PRoyecto
+
+Este projecto en un projecto tipo MERN (Mongodb Express ReactJS NodeJS) aplicando el patron de diseño MVC (Model View Controller), por lo cual el mismo se ha estructurado en las carpetas, Controller, Models, Routes
--- controllers/project.controller.js
+++ controllers/project.controller.js
@@ -1,5 +1,7 @@
 'use strict'
 
+var Project = require('../models/project.models')
+
 var controller = {
 
     home: function(req, res){
@@ -15,6 +17,27 @@ var controller = {
         return res.status(200).send({
             message: 'Soy la Conexion al metodo Post'
         });
+
+    },
+
+    saveProject: function(req, res){
+        var project = new Project();
+
+        var params = req.body;
+        project.name = params.name;
+        project.description = params.description;
+        project.category = params.category;
+        project.year = params.year;
+        project.langs = params.langs;
+        project.image = null;
+
+        project.save((err, projectStored)=> {
+            if (err) return res.status(500).send({ message: 'Error al guardar el documento'});
+            if(!projectStored) return res.status(404).send({ message: 'No se ha podido guardar el projecto'});
+
+            return res.status(200).send({project: projectStored});
+        })
+
 
     }
 };
